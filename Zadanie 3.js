cefce7632ff81aad8c4c8300c91d3f97c5ff145d
Jakub Kizyma <154536112+oZtEpD@users.noreply.github.com>
2024-05-28 12:35:14 +0200

    Add files via upload

---
+++ Zadanie 3.js
@@ -0,0 +1,9 @@
+const fruits = ["Jabłko", "Banan", "Pomarańcza", "Winogrona", "Arbuz"];
+
+const numbers = [26, 15, 32, 18, 0, 5, 1, 3, 4, 8];
+
+for (let i = 0; i < numbers.length; i += 1) {
+  if (numbers[i] > 5) {
+    console.log(numbers[i]);
+  }
+}
